ebfc8f2926f2f40be10b5b35592b7faf91253ea5
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-04-27 16:30:03 +0530

    feat: hide vote result before voting ends

--- src/components/Room/Proposal/Create/index.tsx
+++ src/components/Room/Proposal/Create/index.tsx
@@ -6,6 +6,7 @@ import Title from './Title';
 import Description from './Description';
 import Tags from './Tags';
 import Dates from './Dates';
+import HideResult from './HideResult';
 
 const CreateProposal = () => {
 	return (
@@ -14,6 +15,7 @@ const CreateProposal = () => {
 			<Description />
 			<Tags />
 			<Dates />
+			<HideResult />
 		</section>
 	);
 };
