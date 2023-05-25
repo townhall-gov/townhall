6120b4ffa6546381049fa87dca260b6e7bd26afc
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-05-25 08:20:51 +0530

    feat: about, contact us pages

---
+++ pages/contact-us/index.tsx
@@ -0,0 +1,14 @@
+// Copyright 2019-2025 @polka-labs/townhall authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import React from 'react';
+
+const ContactUs = () => {
+	return (
+		<div className='h-full'>
+			<ContactUs />
+		</div>
+	);
+};
+
+export default ContactUs;
