a808b789f5e73aaf1f6aa726423465a02e229420
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-06-12 13:26:14 +0530

    fix: single proposal ui

--- src/components/Room/Discussion/index.tsx
+++ src/components/Room/Discussion/index.tsx
@@ -7,8 +7,8 @@ import ContentWrapper from './ContentWrapper';
 
 const Discussion = () => {
 	return (
-		<section className='grid grid-cols-12 gap-6 relative w-full'>
-			<ContentWrapper className='col-span-9' />
+		<section className='grid grid-cols-10 gap-6 relative w-full'>
+			<ContentWrapper className='col-span-7' />
 			<Sidebar className='col-span-3' />
 		</section>
 	);
--- src/components/Room/Proposal/index.tsx
+++ src/components/Room/Proposal/index.tsx
@@ -10,9 +10,9 @@ const ProposalWrapper = () => {
 	const { proposal } = useProposalSelector();
 	if (!proposal) return null;
 	return (
-		<section className='flex gap-6 relative w-full'>
-			<ContentWrapper className='flex-1' />
-			<Sidebar className='' />
+		<section className='grid grid-cols-10 gap-6 relative w-full'>
+			<ContentWrapper className='col-span-7' />
+			<Sidebar className='col-span-3' />
 		</section>
 	);
 };
