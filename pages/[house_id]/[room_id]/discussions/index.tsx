ef81bc0b4de550357bf624df32582ceea4f8b461
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-06-17 06:30:06 +0530

    fix: routes fix

--- pages/[house_id]/[room_id]/discussions/index.tsx
+++ pages/[house_id]/[room_id]/discussions/index.tsx
@@ -3,28 +3,42 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React, { FC, useEffect } from 'react';
 import SEOHead from '~src/global/SEOHead';
-import Room from '~src/components/Room';
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
-import { IListingDiscussion } from '~src/redux/room/@types';
+import { ERoomStage, IListingDiscussion } from '~src/redux/room/@types';
 import { useDispatch } from 'react-redux';
 import { roomActions } from '~src/redux/room';
 import { getDiscussions } from 'pages/api/discussions';
+import RoomSidebar from '~src/components/Room/Sidebar';
+import RoomAbout from '~src/components/Room/RoomWrapper/RoomAbout';
+import Discussions from '~src/components/Room/Discussions';
+import { getRoom } from 'pages/api/room';
+import { IRoom } from '~src/types/schema';
+import { useRoomCurrentStage } from '~src/redux/room/selectors';
 
 interface IDiscussionsServerProps {
 	discussions: IListingDiscussion[] | null;
 	error: string | null;
+	room: IRoom | null;
 }
 
 export const getServerSideProps: GetServerSideProps<IDiscussionsServerProps> = async ({ query }) => {
+	const house_id = (query?.house_id? String(query?.house_id): '');
+	const room_id = (query?.room_id? String(query?.room_id): '');
 	const { data, error } = await getDiscussions({
-		house_id: (query?.house_id? String(query?.house_id): ''),
-		room_id: (query?.room_id? String(query?.room_id): '')
+		house_id,
+		room_id
+	});
+
+	const obj = await getRoom({
+		house_id,
+		room_id
 	});
 
 	const props: IDiscussionsServerProps = {
 		discussions: ((data && Array.isArray(data))? (data || []): []),
-		error: (error? error: null)
+		error: (error || obj.error || null),
+		room: obj.data || null
 	};
 
 	return {
@@ -34,10 +48,12 @@ export const getServerSideProps: GetServerSideProps<IDiscussionsServerProps> = a
 
 interface IDiscussionsClientProps extends IDiscussionsServerProps {}
 
-const Discussions: FC<IDiscussionsClientProps> = (props) => {
+const DiscussionsPage: FC<IDiscussionsClientProps> = (props) => {
 	const dispatch = useDispatch();
 	const router = useRouter();
+	const currentStage = useRoomCurrentStage();
 	const { query } = router;
+	const { discussions, room } = props;
 
 	useEffect(() => {
 		if (props.error) {
@@ -45,16 +61,46 @@ const Discussions: FC<IDiscussionsClientProps> = (props) => {
 		} else if (props.discussions && Array.isArray(props.discussions)) {
 			dispatch(roomActions.setDiscussions(props.discussions));
 		}
+		if (props.room) {
+			dispatch(roomActions.setRoom(props.room));
+		}
+		if (currentStage !== ERoomStage.DISCUSSIONS) {
+			dispatch(roomActions.setCurrentStage(ERoomStage.DISCUSSIONS));
+		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [props]);
+
+	console.log(room);
+
+	if (!room) {
+		return null;
+	}
+
 	return (
 		<>
 			<SEOHead title={`Discussions of Room ${query['room_id']} in House ${query['house_id']}`} />
 			<div>
-				<Room />
+				<section className='flex gap-x-7'>
+					<RoomSidebar />
+					<div className='flex-1 flex flex-col gap-y-[21px]'>
+						<section
+							className='flex gap-x-[17.5px]'
+						>
+							<RoomAbout
+								description={room.description}
+								socials={room.socials}
+							/>
+						</section>
+						{
+							discussions && Array.isArray(discussions) && discussions.length > 0 && (
+								<Discussions discussions={discussions} />
+							)
+						}
+					</div>
+				</section>
 			</div>
 		</>
 	);
 };
 
-export default Discussions;
+export default DiscussionsPage;
