2b19abd5d4a397750fc698df8242e6601b95c0c3
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-06-23 08:56:22 +0530

    fix: settings page fetching issue

--- pages/[house_id]/[room_id]/settings/index.tsx
+++ pages/[house_id]/[room_id]/settings/index.tsx
@@ -1,8 +1,10 @@
 // Copyright 2019-2025 @polka-labs/townhall authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { getRoom } from 'pages/api/room';
+import React, { FC, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import RoomSettings from '~src/components/Room/Settings';
 import RoomSidebar from '~src/components/Room/Sidebar';
@@ -11,10 +13,37 @@ import { roomActions } from '~src/redux/room';
 import { ERoomStage } from '~src/redux/room/@types';
 import { useRoomCurrentStage } from '~src/redux/room/selectors';
 import { useRoomSelector } from '~src/redux/selectors';
+import { IRoom } from '~src/types/schema';
 import BackButton from '~src/ui-components/BackButton';
 import NoRoomFound from '~src/ui-components/NoRoomFound';
 
-const SettingsPage = () => {
+interface IRoomSettingsServerProps {
+	room: IRoom | null;
+	error: string | null;
+}
+
+export const getServerSideProps: GetServerSideProps<IRoomSettingsServerProps> = async ({ query }) => {
+	const house_id = (query?.house_id? String(query?.house_id): '');
+	const room_id = (query?.room_id? String(query?.room_id): '');
+
+	const { data: room, error: roomError } = await getRoom({
+		house_id,
+		room_id
+	});
+
+	const props: IRoomSettingsServerProps = {
+		error: (roomError || null),
+		room: room || null
+	};
+
+	return {
+		props: props
+	};
+};
+
+interface IRoomSettingsClientProps extends IRoomSettingsServerProps {}
+
+const RoomSettingsPage: FC<IRoomSettingsClientProps> = (props) => {
 	const dispatch = useDispatch();
 	const currentStage = useRoomCurrentStage();
 	const router = useRouter();
@@ -22,11 +51,14 @@ const SettingsPage = () => {
 	const { query } = router;
 
 	useEffect(() => {
+		if (props.room) {
+			dispatch(roomActions.setRoom(props.room));
+		}
 		if (currentStage !== ERoomStage.SETTINGS) {
 			dispatch(roomActions.setCurrentStage(ERoomStage.SETTINGS));
 		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [props]);
 
 	if (!room) {
 		return <NoRoomFound />;
@@ -44,4 +76,4 @@ const SettingsPage = () => {
 	);
 };
 
-export default SettingsPage;
+export default RoomSettingsPage;
