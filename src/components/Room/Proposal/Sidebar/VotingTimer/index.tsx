98097dd522f73eb5dae775a571d495a33fbcbdc6
Hitesh-gurnani <hiteshgurnani850@gmail.com>
2023-06-27 14:23:19 +0530

    further optimization

--- src/components/Room/Proposal/Sidebar/VotingTimer/index.tsx
+++ src/components/Room/Proposal/Sidebar/VotingTimer/index.tsx
@@ -9,19 +9,19 @@ import dayjs from 'dayjs';
 const VotingTimer = () => {
 	const { proposal } = useProposalSelector();
 	const [ votingTimer,setVotingTimer ] = useState({ countDownVisbility:true,days:0,hours:0,minutes:0,seconds:0 });
-	const countDownUpdation = () => {
+	useEffect(() => {
 		if(proposal)
 		{
 			const { start_date }=proposal;
 			const end = dayjs( start_date );
 			const updateCountdown = () => {
-				const remaining = end.diff(dayjs());
-				if(remaining<=0)
-				{
+				const remaining = end.diff( dayjs() );
+				if( remaining<=0 ) {
+					clearInterval( countdownInterval );
 					setVotingTimer({ ...votingTimer,countDownVisbility:false });
 				}
 				else {
-					const duration = dayjs.duration(remaining);
+					const duration = dayjs.duration( remaining );
 					const days = duration.days();
 					const hours = duration.hours();
 					const minutes = duration.minutes();
@@ -35,47 +35,49 @@ const VotingTimer = () => {
 				}
 			};
 			updateCountdown();
-			if(votingTimer.countDownVisbility)
-			{
-				const countdownInterval = setInterval(updateCountdown, 1000);
-				return () => {
-					clearInterval(countdownInterval);
-				};
-			}
+			const countdownInterval = setInterval( updateCountdown, 1000 );
+			return () => {
+				clearInterval( countdownInterval );
+			};
 		}
-	};
-	useEffect(() => {
-		countDownUpdation();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	},[proposal]);
 	return (
 		<>
-			{votingTimer.countDownVisbility && (
+			{ votingTimer.countDownVisbility && (
 				<section className='border border-solid border-blue_primary rounded-2xl drop-shadow-[0px_6px_18px_rgba(0,0,0,0.06)] p-6 text-white'>
 					<h2 className='font-bold text-lg leading-[22px] tracking-[0.01em] mb-[21px]'>
 				Voting Starts in
 					</h2>
-					<div className='flex flex-col text-[#66A5FF] items-center justify-between'>
-						<div className='flex justify-between text-[32px] text-[#66A5FF]'>
-							<p className='flex flex-col justify-center font-bold items-center text-[32px]'>
-								{votingTimer.days}
-								<span className='text-[14px] gap-y-[5px] font-normal'>Days</span>
-							</p>
-							<div className='mx-2'>:</div>
-							<p className='flex flex-col justify-center font-bold items-center text-[32px]'>
-								{votingTimer.hours}
-								<span className='text-[14px] gap-y-[5px] font-normal'>Hours</span>
-							</p>
-							<div  className='mx-2'>:</div>
-							<p className='flex flex-col justify-center font-bold items-center text-[32px]'>
-								{votingTimer.minutes}
-								<span className='text-[14px] gap-y-[5px] font-normal'>Minutes</span>
-							</p>
-							<div  className='mx-2'>:</div>
-							<p className='flex flex-col justify-center font-bold items-center text-[32px]'>
-								{votingTimer.seconds}
-								<span className='text-[14px] gap-y-[5px] font-normal'>Seconds</span>
-							</p>
-						</div>
+					<div className='flex justify-between text-[32px] text-[#66A5FF] mx-2'>
+						<article className='flex flex-col gap-y-5 items-center'>
+							<p className='font-bold'> {votingTimer.days} </p>
+							<span className='text-sm font-normal'> Days </span>
+						</article>
+						<article className='flex flex-col gap-y-5'>
+							<p className='font-bold'> : </p>
+							<span className='text-sm font-normal'></span>
+						</article>
+						<article className='flex flex-col gap-y-5 items-center'>
+							<p className='font-bold'> {votingTimer.hours} </p>
+							<span className='text-sm font-normal'> Hours </span>
+						</article>
+						<article className='flex flex-col gap-y-5'>
+							<p className='font-bold'> : </p>
+							<span className='text-sm font-normal'></span>
+						</article>
+						<article className='flex flex-col gap-y-5 items-center'>
+							<p className='font-bold'> {votingTimer.minutes} </p>
+							<span className='text-sm font-normal'> Minutes </span>
+						</article>
+						<article className='flex flex-col gap-y-5'>
+							<p className='font-bold'>:</p>
+							<span className='text-sm font-normal'></span>
+						</article>
+						<article className='flex flex-col gap-y-5 items-center'>
+							<p className='font-bold'> {votingTimer.seconds} </p>
+							<span className='text-sm font-normal'> Seconds </span>
+						</article>
 					</div>
 				</section>
 			)}
--- src/components/Room/Proposal/Sidebar/index.tsx
+++ src/components/Room/Proposal/Sidebar/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import classNames from 'classnames';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import Timestamp from './Timestamp';
 import Vote from './Vote';
 import PostLinking from './PostLink';
@@ -17,20 +17,12 @@ interface ISidebarProps {
 const Sidebar: FC<ISidebarProps> = (props) => {
 	const { className } = props;
 	const { proposal } = useProposalSelector();
-	const [startDate,setStartDate]=useState<any>();
-	useEffect(()=>{
-		if(proposal)
-		{
-			const { start_date }=proposal;
-			setStartDate(dayjs(start_date));
-		}
-	},[]);
 	return (
 		<div className={classNames('flex flex-col gap-y-6 sticky top-6 min-w-[350px]', className)}>
 			<PostLinking />
 			<Vote />
 			<Timestamp />
-			{ startDate?.isAfter(dayjs())? <VotingTimer /> : null }
+			{ proposal?.start_date && dayjs(proposal?.start_date)?.isAfter(dayjs())? <VotingTimer /> : null }
 		</div>
 	);
 };
